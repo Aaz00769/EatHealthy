a65709cffe5733f5ebb1f1edda788475ec73c4f8
azaha <azaha@DESKTOP-8H1757Q>
2025-07-21 19:41:28 +0300

    IProductInterface added : GetAllDelitedProductAsync SoftDeletProductAsync HardDeleteProductAsync RestoreProductAsync

--- AspNetCoreArchTemplate.Services.Core/Interfaces/IProductService.cs
+++ AspNetCoreArchTemplate.Services.Core/Interfaces/IProductService.cs
@@ -1,23 +1,32 @@
-﻿using EatHealthy.Web.ViewModels.Product;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace EatHealthy.Services.Core.Interfaces
 {
+    using EatHealthy.Web.ViewModels.Product;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
     public interface IProductService
     {
         Task<IEnumerable<ProductViewModel>> GetAllProductAsync();
+
+        Task<IEnumerable<ProductViewModel>> GetAllDelitedProductAsync();
 
         Task AddProductAsync(ProductFormInputModel inputModel);
 
         Task<bool> ProductExist(string name);
 
         Task EditProductAsync(Guid id, ProductFormInputModel inputModel);
+
         Task<ProductFormInputModel?> GetProductByIdAsync(Guid id);
         
+        Task<bool> SoftDeletProductAsync(Guid id);
+
+        Task<bool> HardDeleteProductAsync(Guid id);
+
+        Task<bool> RestoreProductAsync(Guid id);
 
     }
 }
