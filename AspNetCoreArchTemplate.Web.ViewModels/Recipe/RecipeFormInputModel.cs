50dff4a21b10b27f5e567904ba2c5869f2633abf
azaha <azaha@DESKTOP-8H1757Q>
2025-07-21 19:53:55 +0300

    Added : RecepieFormInputModel RecipeProductInputModel RecipeViewModel

---
+++ AspNetCoreArchTemplate.Web.ViewModels/Recipe/RecipeFormInputModel.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace EatHealthy.Web.ViewModels.Recipe
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using static EatHealthy.Web.ViewModels.ValidationMessages.Recipe;
+
+    public class RecipeFormInputModel
+    {
+        public Guid? RecipeId { get; set; }
+
+        [Required(ErrorMessage =NameRequired)]
+        [StringLength(100, ErrorMessage = NameLength)]
+        public string Name { get; set; } = null!;
+
+        [Url(ErrorMessage = InvalidUrl)]
+        public string? ImageUrl { get; set; }
+
+        [StringLength(1000, ErrorMessage = DescriptionLength)]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = AtLeastOneProduct)]
+        [MinLength(1, ErrorMessage = AtLeastOneProduct)]
+        public List<RecipeProductInputModel> SelectedProducts { get; set; } = new();
+
+
+    }
+}
---
+++ AspNetCoreArchTemplate.Web.ViewModels/Recipe/RecipeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EatHealthy.Web.ViewModels.Recipe
+{
+    public class RecipeViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public List<RecipeProductInputModel> SelectedProducts { get; set; } = new();
+
+        public int TotalCalories { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
