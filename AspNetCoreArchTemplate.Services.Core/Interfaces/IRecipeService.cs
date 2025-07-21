5120c17141fb7a099660dd370b89ab69dde5b0a6
azaha <azaha@DESKTOP-8H1757Q>
2025-07-21 19:42:22 +0300

    Added new ServiceInterface:IRecipeService

---
+++ AspNetCoreArchTemplate.Services.Core/Interfaces/IRecipeService.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace EatHealthy.Services.Core.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using EatHealthy.Web.ViewModels.Recipe;
+    public interface IRecipeService
+    {
+        Task<IEnumerable<RecipeViewModel>> GetAllRecipesAsync();
+        Task<RecipeFormInputModel?> GetByIdAsync(Guid id);
+        Task AddRecipeAsync(RecipeFormInputModel model);
+        Task EditRecipeAsync(Guid id, RecipeFormInputModel model);
+        Task<bool> SoftDeleteRecipeAsync(Guid id);
+    }
+}
